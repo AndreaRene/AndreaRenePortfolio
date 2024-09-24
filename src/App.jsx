14f0aa46c87bef95dd980a1350d34fd9f262c118
AndreaRene <andrea@interhact.net>
2024-09-23 21:00:48 -0500

    bug: weird github pages routing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Navbar/Header';
-import Footer from './components/Footer/Footer';
-import Profile from './pages/Profile/Profile';
-import Portfolio from './pages/Portfolio/Portfolio';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
