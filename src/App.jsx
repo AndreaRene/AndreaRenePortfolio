d13bd73473c1e12534c4623212dd443ec5417653
AndreaRene <andrea@interhact.net>
2024-09-23 21:15:58 -0500

    update to hashrouter again to fix uri issues. Will fix later

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Navbar/Header';
 import Footer from './components/Footer/Footer';
 import Profile from './pages/Profile/Profile';
