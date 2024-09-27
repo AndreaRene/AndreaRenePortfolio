e7bddb590b45d1381a9140f587a30520eda1ca71
AndreaRene <andrea@interhact.net>
2024-09-27 18:05:14 -0500

    update lights panel

--- src/pages/Contact/LightsPanel.jsx
+++ src/pages/Contact/LightsPanel.jsx
@@ -43,21 +43,26 @@ const LightsPanel = () => {
         ></div>
       </div>
       <div className="light-controls">
-        <button className="lights-button" onClick={() => handleButtonClick(1)}>
-          1
-        </button>
-        <button className="lights-button" onClick={() => handleButtonClick(2)}>
-          2
-        </button>
-        <button className="lights-button" onClick={() => handleButtonClick(3)}>
-          3
-        </button>
-        <button className="lights-button" onClick={() => handleButtonClick(4)}>
-          4
-        </button>
-        <button className="lights-button" onClick={() => handleButtonClick(5)}>
-          5
-        </button>
+        <button
+          className="lights-button"
+          onClick={() => handleButtonClick(1)}
+        ></button>
+        <button
+          className="lights-button"
+          onClick={() => handleButtonClick(2)}
+        ></button>
+        <button
+          className="lights-button"
+          onClick={() => handleButtonClick(3)}
+        ></button>
+        <button
+          className="lights-button"
+          onClick={() => handleButtonClick(4)}
+        ></button>
+        <button
+          className="lights-button"
+          onClick={() => handleButtonClick(5)}
+        ></button>
       </div>
 
       {/* Modal for Error */}
